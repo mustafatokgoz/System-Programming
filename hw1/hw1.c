937b4bb385259948b5808422eb627378ecd8fbe2
mustafatokgoz <mustafatokgz@icloud.com>
2022-03-16 02:41:49 +0300

    hala eksik

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -13,7 +13,7 @@ void seperate_argumans(char **,int);
 void file_operations(char **,int, char*);
 int open_file(char *);
 void change_occurance(int , char *, char *,int);
-void helper_op(int *, int *, int *);
+void helper_op(int *, int *, int *, int *);
 
 
 int main(int argc, char **argv){
@@ -41,32 +41,31 @@ void change_occurance(int temp_fd, char *cur_occ, char *occ,int many){
 }
 
 
-void helper_op(int *number_l, int *check, int *check2){
+void helper_op(int *number_l, int *check, int *check2,int *j){
     if(*number_l == 1){
         *check2 = 1;
+        *j = *j - 1;
     }
     *number_l = 0;
     *check = 0;
     
 }
 
-void helper_else(boolean case_insensitive, char cur,char c, int *number_l, int *check, int *check2,int *star,int insense){
+void helper_else(boolean case_insensitive, char cur,char c, int *number_l, int *check, int *check2,int *j,int insense){
     if(case_insensitive == True){
         if ((c == (cur - 32)) || (c == (cur + 32))){
             if(insense == 0){
                 *number_l = *number_l + 1;
                 *check = *check + 1;
             }
-            else{
-                *star = 1;
-            }
         }
-        else{
-            helper_op(number_l,check,check2);
+        else{ 
+            
+            helper_op(number_l,check,check2,j);
         }
     }
     else{
-        helper_op(number_l,check,check2);
+        helper_op(number_l,check,check2,j);
     }
 }
 
@@ -83,16 +82,66 @@ int find_star_number(char *word){
     return number;
 }
 
+void lock_file(int fd, struct flock fl){
+    fl.l_type = F_WRLCK;
+    perror("heyy");
+    if (fcntl(fd, F_SETLKW, &fl) == -1){
+    		perror("fcntl");
+    		exit(1);
+    }   
+    perror("heyy2"); 
+}
+
+void unlock_file(int fd, struct flock fl){
+    fl.l_type = F_UNLCK;
+    if (fcntl(fd, F_SETLKW, &fl) == -1){
+    		perror("fcntl");
+    		exit(1);
+    }    
+}
+
+void set_to_begin(int fd){
+    if(lseek(fd,0,SEEK_SET) == -1){
+            exitInf("lseek error");
+    }
+}
+
+char *get_content(char *content,int fd){
+    int rd,count = 0 , i = 0;
+    char c;
+    if(lseek(fd,0,SEEK_SET) == -1){
+            exitInf("lseek error");
+    }
+    rd = read(fd, &c, 1);
+    count++;
+    while(rd > 0){
+        rd = read(fd, &c, 1);
+        count++;
+    }
+    content = malloc(count * sizeof(char));
+    if(lseek(fd,0,SEEK_SET) == -1){
+            exitInf("lseek error");
+    }
+
+    for(i = 0; i < count - 1  ; i++){
+        read(fd, &c, 1);
+        content[i] = c;
+    }
+    return content;
+}
+
 void file_operations(char **arr, int count, char *file_name){
     int fd,temp_fd;
     char c;
-    int i;
+    int i,j;
     int rd;
     char last_c;
     char temp_file[]="/tmp/my-temp-fileXXXXXX";
     boolean case_insensitive = False;
     int number_l = 0, check = 0, check2 = 0, star = 0, star_number = 0;
     struct flock fl = { F_WRLCK, SEEK_SET, 0,       0,     0 };
+    char *content;
+    int begin_check=0;
 
     fd = open_file(file_name);
 
@@ -105,7 +154,7 @@ void file_operations(char **arr, int count, char *file_name){
     }
 
     fl.l_pid    = getpid();
-
+    
     
     for(i = 0; i < count; i = i + 2){
         if ((i+2) < count && len(arr[i+2]) == 1 && arr[i+2][0] == 'i'){
@@ -118,15 +167,25 @@ void file_operations(char **arr, int count, char *file_name){
         check2 = 0;
         number_l = 0;
 
+        lock_file(fd,fl);
+        /*
         if (fcntl(fd, F_SETLKW, &fl) == -1){
     		perror("fcntl");
     		exit(1);
         }    
+        
+        */
 
         //write(1, "it will be lock\n" , len("it will be lock\n"));
         //write(1, "Press any to release lock: ", len("Press any to release lock: "));
         //getchar();
- 
+       ;
+        content = get_content(content,fd);
+
+        set_to_begin(fd);
+        set_to_begin(temp_fd);
+
+        /* 
         if(lseek(fd,0,SEEK_SET) == -1)
         {
             exitInf("lseek error");
@@ -136,6 +195,57 @@ void file_operations(char **arr, int count, char *file_name){
         {
             exitInf("lseek error");
         }
+
+        */
+       perror("heyy");
+        write(1,content,len(content));
+        write(1,content,len(content));
+        for(j = 0; j < len(content); j++){
+            if(content[j] == arr[i][check]){
+                number_l ++;
+                check ++;
+            }
+            else if (arr[i][check] == '^'){
+                if(j == 0){
+                    number_l ++;
+                    j--;
+                }
+                if(content[j] == '\n'){
+                    perror("gggg1   ");
+                    number_l ++;
+                    write(temp_fd, &content[j], 1);
+                    
+                }
+                else{
+                    helper_else(case_insensitive,arr[i][check],content[j],&number_l,&check,&check2,&j,0);
+                }
+                check ++;
+            }
+            else if (arr[i][check] == '$'){
+                if(content[j]=='\n'){
+                    number_l ++;
+                }
+                check = 0;
+            }
+            else{
+                helper_else(case_insensitive,arr[i][check],content[j],&number_l,&check,&check2,&j,0);
+            }
+
+
+            if (number_l == len(arr[i])){
+                change_occurance(temp_fd,arr[i],arr[i+1],len(arr[i]));
+            }
+            else{
+                if(check2 == 0) {
+                        write(temp_fd, &content[j], 1);
+                }
+            }
+
+            check2 = 0;
+
+        }
+
+        /* 
         rd = read(fd, &c, 1);
         while(rd > 0){
             if (c == arr[i][check]){
@@ -188,25 +298,25 @@ void file_operations(char **arr, int count, char *file_name){
             }
             
         }
-
-        write(1," ! buraya ! ",len(" ! buraya ! "));
-        fl.l_type = F_UNLCK;  /* set to unlock same region */
-
-        if (fcntl(fd, F_SETLK, &fl) == -1) {
-    	    perror("fcntl");
-    	    exit(1);
-        }
-
+        */
         ftruncate(fd, 0);
+
+
         //close(fd);
 
         //fd=open(file_name,O_RDWR | O_TRUNC);
-
+        /* 
         if (fcntl(fd, F_SETLKW, &fl) == -1){
     		perror("fcntl");
     		exit(1);
         }
 
+        */
+        
+        set_to_begin(fd);
+        set_to_begin(temp_fd);
+
+        /* 
         if(lseek(fd,0,SEEK_SET) == -1)
         {
             exitInf("lseek error");
@@ -215,6 +325,8 @@ void file_operations(char **arr, int count, char *file_name){
         {
             exitInf("lseek error");
         }
+
+        */
         rd = read(temp_fd, &c, 1);
         while(rd > 0){
             write(fd,&c,1);
@@ -224,47 +336,20 @@ void file_operations(char **arr, int count, char *file_name){
         if (case_insensitive == True){
             i++;
         }
-
-        fl.l_type = F_UNLCK;  /* set to unlock same region */
+        /* 
+        fl.l_type = F_UNLCK;  // set to unlock same region 
     
         if (fcntl(fd, F_SETLK, &fl) == -1) {
     	    perror("fcntl");
     	    exit(1);
         }
+
+        */
+
+        unlock_file(fd,fl);
+
         ftruncate(temp_fd, 0);
     }
-
-    
-    /* 
-    rd = read(fd, &c, 1);
-    while(c!='\n' && rd > 0){
-        write(1,&c,1);
-        rd = read(fd, &c, 1);
-    }
-    if(lseek(fd,0,SEEK_SET) == -1)
-    {
-        exitInf(errno);
-    }
-    write(fd,"deneme123",len("deneme123"));
-    close(fd);
-    fd=open(file_name,O_RDWR | O_TRUNC);
-    if(lseek(fd,0,SEEK_SET) == -1)
-    {
-        exitInf(errno);
-    }
-    write(fd,"deneme123",len("deneme123"));
-
-    if(lseek(fd,0,SEEK_SET) == -1)
-    {
-        exitInf(errno);
-    }
-    rd = read(fd, &c, 1);
-    while(c!='\n' && rd > 0){
-        write(1,&c,1);
-        rd = read(fd, &c, 1);
-    }
-    write(1,"\n",1);
-    */
     close(fd);
 
 }
