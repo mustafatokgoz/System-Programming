a6ea1f8bc4b6719a17618ce34cb0402174c2d609
mustafatokgoz <mustafatokgz@icloud.com>
2022-03-18 02:27:02 +0300

    zip

---
+++ hw1/171044077/helper.c
@@ -0,0 +1,32 @@
+#include "helper.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int len(char *str){
+    int i = 0;
+    for(i = 0; str[i] !='\0'; i++);
+    return i;
+}
+
+void errExit(char *inf){
+    char *errInf = "Error was occured for given information : ";
+    char *errInf2 ="Example usage : ./hw1 \'/^Window[sz]*/Linux/i;/close[dD]$/open/\' inputFilePath";
+    write(2, errInf, len(errInf));
+    write(2, inf, len(inf));    
+    write(2, "\n", 1);
+    write(2,errInf2, len(errInf2));
+    write(2, "\n", 1);
+    exit(0);
+}
+
+
+void exitInf(char *err){
+    perror(err);
+    exit(0);
+}
+
+
+
